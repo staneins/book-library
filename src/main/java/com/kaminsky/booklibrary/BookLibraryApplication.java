7de1d88c970275f97f3f2eded3fb8cfdd2d4fa46
Philip Kaminsky <kaminsky8836@gmail.com>
2024-12-05 14:43:00 +0300

    initial commit

---
+++ src/main/java/com/kaminsky/booklibrary/BookLibraryApplication.java
@@ -0,0 +1,13 @@
+package com.kaminsky.booklibrary;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BookLibraryApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BookLibraryApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/kaminsky/booklibrary/BookLibraryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kaminsky.booklibrary;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BookLibraryApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
